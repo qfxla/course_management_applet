eaefefbb2dccfdbfc857bb655e478cdef8a6e49b
JingTao <834616898@qq.com>
2021-11-06 10:53:29 +0800

    新的项目

---
+++ src/main/java/com/haotongxue/course_service/config/Swagger3Config.java
@@ -0,0 +1,70 @@
+package com.haotongxue.course_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import springfox.documentation.builders.*;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Configuration
+@EnableOpenApi //注解启动用Swagger的使用，同时在配置类中对Swagger的通用参数进行配置
+public class Swagger3Config {
+    @Bean
+    public Docket createRestApi(){
+        //返回文档概要信息
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zktiaozao.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                //.globalRequestParameters(getGlobalRequestParameters())
+                .globalResponses(HttpMethod.GET,getGlobalResponseMessage())
+                .globalResponses(HttpMethod.POST,getGlobalResponseMessage());
+    }
+
+    /*
+    生成接口信息，包括标题，联系人等
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("仲恺跳蚤后端")
+                .description("一次编码处处停运")
+                .contact(new Contact("邓景涛","localhost","834616898@qq.com"))
+                .version("1.0")
+                .build();
+    }
+
+
+    /*
+    封装全局通用参数
+     */
+//    private List<RequestParameter> getGlobalRequestParameters() {
+//        List<RequestParameter> parameters=new ArrayList<>();
+//        parameters.add(new RequestParameterBuilder()
+//                .name("uuid")
+//                .description("设备uuid")
+//                .required(true)
+//                .in(ParameterType.QUERY)
+//                .query(q->q.model(m->m.scalarModel((ScalarType.STRING))))
+//                .required(false)
+//                .build());
+//
+//        return parameters;
+//    }
+    /*
+    封装通用相应信息
+     */
+    private List<Response> getGlobalResponseMessage() {
+        List<Response> responseList=new ArrayList<>();
+        responseList.add(new ResponseBuilder().code("404").description("未找到资源").build());
+        return responseList;
+    }
+}
