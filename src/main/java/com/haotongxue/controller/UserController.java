a4bb689ece0789a533370f8cec5df5c1314a09f5
JingTao <834616898@qq.com>
2021-11-08 13:19:35 +0800

    完善了登录逻辑

--- src/main/java/com/haotongxue/controller/UserController.java
+++ src/main/java/com/haotongxue/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.haotongxue.controller;
 
 
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.haotongxue.entity.User;
@@ -20,6 +24,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * <p>
@@ -45,22 +51,52 @@ public class UserController {
 
     @ApiOperation(value = "微信登录")
     @PostMapping("/login")
-    public R login(@RequestBody WeChatLoginDTO loginDTO){
+    public R login(@RequestBody WeChatLoginDTO loginDTO) throws IOException {
         WeChatLoginResponse loginResponse = userService.getLoginResponse(loginDTO.getCode());
         String openid = loginResponse.getOpenid();
         User user = (User) cache.get("logi" + openid);
-        boolean isDoPa = true; //是否执行校园网登录验证
-        HtmlPage page = null;
+        boolean isDoPa = true; //是否执行学校系统登录验证
         if (user == null){
             //快捷登录失败
             if (loginDTO.getNickName() == null || loginDTO.getNo() == null || loginDTO.getPassword() == null){
                 return R.error().code(ResultCode.QUICK_LOGIN_ERROR);
             }
-            page = eduLoginService.login(loginDTO.getNo(), loginDTO.getPassword());
-            //学校官网登录失败
-            if (page == null){
+            //执行官网的登录
+            WebClient webClient = new WebClient();
+            //配置webClient
+            webClient.getOptions().setCssEnabled(false);
+            webClient.getOptions().setJavaScriptEnabled(true);
+            webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+            webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            webClient.waitForBackgroundJavaScript(3*1000);
+            webClient.getCookieManager().setCookiesEnabled(true);
+
+            //执行表单提交
+            HtmlPage page = webClient.getPage("http://edu-admin.zhku.edu.cn");
+            HtmlForm loginForm = page.getHtmlElementById("loginForm");
+            HtmlInput username = loginForm.getInputByName("username");
+            username.setValueAttribute(loginDTO.getNo());
+            HtmlInput password = loginForm.getInputByName("password");
+            password.setValueAttribute(loginDTO.getPassword());
+            HtmlElement submit = page.getHtmlElementById("submit");
+            HtmlPage afterClick = submit.click();
+            webClient.waitForBackgroundJavaScript(1000);
+
+            boolean isSuccessLogin = false;
+            try {
+                afterClick.getHtmlElementById("loginForm");
+                //如果没有找到元素则抛出异常（证明loginForm1不是登录页，账号和密码正确）
+            }catch (ElementNotFoundException notFoundException){
+                notFoundException.printStackTrace();
+                //标记登录成功
+                isSuccessLogin = true;
+            }
+
+            if (!isSuccessLogin){
                 return R.error().code(ResultCode.NO_OR_PASSWORD_ERROR);
             }
+
             user = new User();
             BeanUtils.copyProperties(loginDTO,user);
             user.setOpenid(openid);
