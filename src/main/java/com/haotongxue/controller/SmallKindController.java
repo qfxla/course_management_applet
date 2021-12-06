9f1093fe4ce00755dad54e0cd1573872bd188ca3
cj杰 <9050812+a2690357721@user.noreply.gitee.com>
2021-12-06 23:03:27 +0800

    生成代码3

---
+++ src/main/java/com/haotongxue/controller/SmallKindController.java
@@ -0,0 +1,21 @@
+package com.haotongxue.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author DJT
+ * @since 2021-12-06
+ */
+@RestController
+@RequestMapping("/smallKind")
+public class SmallKindController {
+
+}
+
