b462762f242c3f5f5f78f1bbdb641ede7f6b5e24
cj杰 <9050812+a2690357721@user.noreply.gitee.com>
2021-12-12 11:25:00 +0800

    a

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,24 @@
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.haotongxue.Main9001;
+import com.haotongxue.entity.College;
+import com.haotongxue.service.ICollegeService;
+import com.haotongxue.utils.WhichCollege;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Main9001.class)
 public class Test {
-    public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
+    @Autowired
+    ICollegeService iCollegeService;
+
+    @org.junit.Test
+    public void test(){
+//        WhichCollege whichCollege = new WhichCollege();
+        int collegeId = WhichCollege.getCollegeId("202010244130");
+        System.out.println("hh" + collegeId);
     }
 }
