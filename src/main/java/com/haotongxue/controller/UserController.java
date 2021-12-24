c0d5c75b7378df63451dcd969e9251af6f90d460
陈铜川 <ctc11111@163.com>
2021-12-24 22:37:25 +0800

    改了一点儿

--- src/main/java/com/haotongxue/controller/UserController.java
+++ src/main/java/com/haotongxue/controller/UserController.java
@@ -384,14 +384,14 @@ public class UserController {
 
     @ApiOperation("学生评教")
     @PostMapping("/evaluate")
-//    public R studentEvaluate(@RequestHeader("Authority") @ApiParam("放在请求头的token") String authority){
-    public R studentEvaluate(){
-//        String currentOpenid = UserContext.getCurrentOpenid();
-//        User user = (User) cache.get(currentOpenid);
-        String no = "202010244304";
-        String password = "Ctc779684470...";
-//        String no = user.getNo();
-//        String password = user.getPassword();
+    public R studentEvaluate(@RequestHeader("Authority") @ApiParam("放在请求头的token") String authority){
+//    public R studentEvaluate(){
+        String currentOpenid = UserContext.getCurrentOpenid();
+        User user = (User) cache.get(currentOpenid);
+//        String no = "202010244304";
+//        String password = "Ctc779684470...";
+        String no = user.getNo();
+        String password = user.getPassword();
         for (int i=0;i<10;i++){
             WebClient webClient = WebClientUtils.getWebClient();
             try {
